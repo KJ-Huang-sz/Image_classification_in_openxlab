1215b47bbb1833718d6297519fe31aa240ae5495
KJ_Huang <33456576+KJ-Huang-sz@users.noreply.github.com>
2024-01-15 20:18:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,13 @@
 import gradio as gr
-import torch
-import requests
-from torchvision import transforms
+import os
 
-torch.hub._validate_not_a_forked_repo=lambda a,b,c: True
-model = torch.hub.load('pytorch/vision:v0.6.0', 'resnet18', pretrained=True).eval()
-response = requests.get("https://git.io/JJkYN")
-labels = response.text.split("\n")
+# 输入一张图片，旋转45°后输出
+def image_mod(image):
+    return image.rotate(45)
 
-def predict(inp):
-  inp = transforms.ToTensor()(inp).unsqueeze(0)
-  with torch.no_grad():
-    prediction = torch.nn.functional.softmax(model(inp)[0], dim=0)
-    confidences = {labels[i]: float(prediction[i]) for i in range(1000)}    
-  return confidences
 
-demo = gr.Interface(fn=predict, 
-             inputs=gr.inputs.Image(type="pil"),
-             outputs=gr.outputs.Label(num_top_classes=3),
-             examples=[["cheetah.jpg"]],
-             )
-             
-demo.launch()
+demo = gr.Interface(image_mod, gr.Image(type="pil"), "image",
+    allow_flagging="never")
+
+if __name__ == "__main__":
+    demo.launch()
